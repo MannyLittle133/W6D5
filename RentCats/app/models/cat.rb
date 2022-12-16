0b259b77c93b6fb7eb8da19442e14fb9c813422a
Wilson Wu <wuyecheng738@gmail.com>
2022-12-16 16:47:01 -0500

    datetime

--- RentCats/app/models/cat.rb
+++ RentCats/app/models/cat.rb
@@ -1,3 +1,35 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Cat < ApplicationRecord
-    
+  validates :id, :birth_date, :color, :name, :sex, presence: true
+  CAT_COLORS = [:orange, :black, :white, :blue, :grey, :green, :yellow, :brown]
+  SEX = [:M, :F]
+  def inclusion
+    if !CAT_COLORS.include?(color)
+      errors.add(:color, 'color not presence')
+    end
+
+    if !SEX.include(sex)
+      errors.add(:sex, 'must choose M or F')
+    end
+  end
+
+  def birth_date_cannot_be_future
+    if Date.parse(birth_date, '%Y-%m-%d')  > Date.today
+      errors.add(:birth_date, 'not a valid date')
+    end
+  end
+
+  
 end
